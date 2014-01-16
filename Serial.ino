93467d3f152745423a155ce580950e14e6be2fc4
mstravendev <mstravendev@gmail.com>
2014-01-16 00:04:42 +0000

    Serial Read for RTC, Light, Dosing, Temp, PH

--- Serial.ino
+++ Serial.ino
@@ -14,21 +14,45 @@ void serialEvent() {
 }
 
 void serialHandler(String input){
-  int fIndex = slitString(input,':',0).toInt();
-  String fValue= slitString(input,':',1);
+  int fIndex = slitString(input,'#',0).toInt();
+  String fValue= slitString(input,'#',1);
+      char t_char[sizeof(slitString(input,'#',2))];
+    int adr;
+  
   switch(fIndex){
-    // Time to set => Format DD/MM/YYYY/H/i
+    // Time to set => Format 1#DD/MM/YYYY/H/i
     case 1:
       rtc.adjust(slitString(fValue,'/',2).toInt(),slitString(fValue,'/',1).toInt(),slitString(fValue,'/',0).toInt(),slitString(fValue,'/',3).toInt(),slitString(fValue,'/',4).toInt(),0);
     break;
-    // Light Chanel
+    // Light Chanel => Format 2#0#0:01=0,11:00=0,13:00=50,20:00=50,22:00=0,23:30=0,23:30=0,23:32=0
     case 2:
+      writeLightArr(slitString(input,'#',2),fValue.toInt());
+      adr = eepromLight +(sizeof(s_lightVal)*fValue.toInt());
+      slitString(input,'#',2).toCharArray(t_char,sizeof(s_lightVal));
+      EEPROM.updateBlock(adr, t_char,sizeof(s_lightVal));
+      EEPROM.updateByte(0, overwrite);
     break;
-    // Dosing
+    // Dosing => Format 3#1=NPK=10:00=5=60
     case 3:
+       writePumpArr(slitString(input,'#',2),fValue.toInt());
+      adr = eepromDosing +(sizeof(s_dosingVal)*fValue.toInt());
+      slitString(input,'#',2).toCharArray(t_char,sizeof(s_dosingVal));
+      EEPROM.updateBlock(adr, t_char,sizeof(s_dosingVal));
+      EEPROM.updateByte(1, overwrite);
     break;
-    // Temp
+    // Temp => Format 4#30
     case 4:
+    coolingTemp=fValue.toInt();
+    EEPROM.updateInt(eepromTemp, coolingTemp);
+    EEPROM.updateByte(2, overwrite);
+    break;
+    // PH => Format 5#7.00
+    case 5:
+    char t_char[sizeof(fValue)];
+    fValue.toCharArray(t_char,sizeof(s_lightVal));
+    phValue=atof(t_char);
+    EEPROM.updateDouble(eepromPH, phValue);
+    EEPROM.updateByte(3, overwrite);
     break;
   }
     
